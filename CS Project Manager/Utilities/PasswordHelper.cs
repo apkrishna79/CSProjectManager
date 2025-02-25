7b9e93d2f560fe840f4825108634395b29b8995c
JazzyFierce <102490072+JazzyFierce@users.noreply.github.com>
2025-02-25 13:41:00 -0600

    Password helper

---
+++ CS Project Manager/Utilities/PasswordHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace CS_Project_Manager.Utilities
+{
+    public class PasswordHelper
+    {
+        // Hashes a plaintext password using BCrypt for secure storage
+        public static string HashPassword(string password) =>
+            BCrypt.Net.BCrypt.HashPassword(password);
+
+        // Verifies a plaintext password against a hashed password to check for a match
+        public static bool VerifyPassword(string password, string hash) =>
+            BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
