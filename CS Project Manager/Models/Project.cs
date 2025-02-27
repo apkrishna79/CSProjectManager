0561c431efe78a294492216cd588f70befab6f4a
jwund <jwunderlich@ku.edu>
2025-02-27 01:43:13 -0600

    Project model, Project service, create project page and button

---
+++ CS Project Manager/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS_Project_Manager.Models
+{
+    public class Project
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Project name cannot exceed 100 characters")]
+        public required string project_name { get; set; }
+
+        [Required]
+        public required string description { get; set; }
+
+        public List<ObjectId> StudentMembers { get; set; } = [];
+    }
+}
