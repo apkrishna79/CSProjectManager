531875b9f845f466e0820a6bd1bfb2e48cfa1eb1
JazzyFierce <102490072+JazzyFierce@users.noreply.github.com>
2025-03-17 08:06:58 -0500

    Models

---
+++ CS Project Manager/Models/DiscussionBoard.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace CS_Project_Manager.Models
+{
+    public class DiscussionBoard
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        public bool IsClassBoard;
+
+        // null if team board
+        [BsonElement("ClassId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId ClassId { get; set; }
+
+        // null if class board
+        [BsonElement("TeamId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId TeamId { get; set; }
+    }
+}
---
+++ CS Project Manager/Models/DiscussionPost.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace CS_Project_Manager.Models
+{
+    public class DiscussionPost
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("BoardId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId BoardId { get; set; }
+
+        [BsonElement("PosterId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId PosterId { get; set; }
+
+        public string? Title { get; set; }
+
+        public required string Content;
+
+        public DateTime Timestamp;
+
+        [BsonElement("ParentPostId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId ParentPostId { get; set; }
+
+        public bool IsReply { get; set; }
+    }
+}
