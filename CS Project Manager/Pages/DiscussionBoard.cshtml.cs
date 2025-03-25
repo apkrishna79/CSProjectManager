e4bb766abd83b16bab444a78fd98ebf01f5529bb
JazzyFierce <102490072+JazzyFierce@users.noreply.github.com>
2025-03-25 15:03:01 -0500

    List posts on discussion board

--- CS Project Manager/Pages/DiscussionBoard.cshtml.cs
+++ CS Project Manager/Pages/DiscussionBoard.cshtml.cs
@@ -1,21 +1,30 @@
 using CS_Project_Manager.Models;
+using CS_Project_Manager.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MongoDB.Bson;
 
 namespace CS_Project_Manager.Pages
 {
-    public class DiscussionBoardModel : PageModel
+    public class DiscussionBoardModel(DiscussionPostService discussionPostService) : PageModel
     {
+        private readonly DiscussionPostService _discussionPostService = discussionPostService;
+
         public ObjectId BoardId { get; set; }
 
-        public void OnGet(string boardId)
+        [BindProperty]
+        public List<DiscussionPost> LinkedPosts { get; set; } = [];
+
+        public async Task OnGetAsync(string boardId)
         {
             if (!ObjectId.TryParse(boardId, out ObjectId parsedBoardId))
             {
                 throw new ArgumentException("Invalid project ID format");
             }
             BoardId = parsedBoardId;
+            LinkedPosts = (await _discussionPostService.GetDiscussionPostsByBoardId(BoardId))
+                .Where(post => !post.IsReply)
+                .ToList();
         }
     }
 }
