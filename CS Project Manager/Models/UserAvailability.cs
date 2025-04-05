dfcc9d42cc34bcd1eee1f8854ad18d068261c8f6
Ginny Ke <130810135+Ginnyk10@users.noreply.github.com>
2025-04-04 19:36:27 -0500

    add comments to user availability model

--- CS Project Manager/Models/UserAvailability.cs
+++ CS Project Manager/Models/UserAvailability.cs
@@ -1,9 +1,17 @@
-﻿using MongoDB.Bson;
+/*
+* Prologue
+Created By: Anakha Krishna
+Date Created: 3/1/25
+Last Revised By: Ginny Ke - added comments
+Date Revised: 4/4/25
+Purpose: model to handle user availability for calendar and meetings
+*/
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace CS_Project_Manager.Models
 {
-    public class UserAvailability
+    public class UserAvailability// create public class with ObjectId, day, time, userid, and project id passed in to database
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
