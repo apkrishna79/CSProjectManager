f31d6b3374311429fbadd12f6fc96a9b49c8f7e7
Ginny Ke <130810135+Ginnyk10@users.noreply.github.com>
2025-04-04 19:49:11 -0500

    added comments to calendaritem model

--- CS Project Manager/Models/CalendarItem.cs
+++ CS Project Manager/Models/CalendarItem.cs
@@ -1,10 +1,18 @@
-﻿using MongoDB.Bson;
+/*
+* Prologue
+Created By: Anakha Krishna
+Date Created: 3/1/25
+Last Revised By: Ginny Ke - added comments
+Date Revised: 4/4/25
+Purpose: model to handle calendar items in databse
+*/
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.ComponentModel.DataAnnotations;
 
-namespace CS_Project_Manager.Models
+namespace CS_Project_Manager.Models 
 {
-    public class CalendarItem
+    public class CalendarItem // creates calendaritem class that passes in object id, event name, state date time, end date time, assoc team id, and notes. 
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
