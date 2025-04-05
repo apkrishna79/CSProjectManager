987ca8084110cd388a3c5623011b7a959125a08a
Ginny Ke <130810135+Ginnyk10@users.noreply.github.com>
2025-04-04 19:42:26 -0500

    Update Requirement.cs

--- CS Project Manager/Models/Requirement.cs
+++ CS Project Manager/Models/Requirement.cs
@@ -1,10 +1,19 @@
+/*
+* Prologue
+Created By: Anakha Krishna
+Date Created: 3/1/25
+Last Revised By: Ginny Ke - added comments
+Date Revised: 4/4/25
+Purpose: model to handle requirements in the database
+*/
+
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.ComponentModel.DataAnnotations;
 
 namespace CS_Project_Manager.Models
 {
-    public class Requirement
+    public class Requirement // creates requirements class that passes in object id, requirementid, description, priority, storypoints, sprintno, assignees, projectid, and isComplete.
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
