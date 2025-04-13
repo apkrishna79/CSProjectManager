148e77ccb09a9db1e79cf80ed71db5a2c991a9e6
jwund <jwunderlich@ku.edu>
2025-04-13 14:01:33 -0500

    meeting minutes model and service

---
+++ CS Project Manager/Models/MeetingMinutes.cs
@@ -0,0 +1,28 @@
+/*
+* Prologue
+Created By: Jackson Wunderlich
+Date Created: 4/13/25
+Last Revised By: ackson Wunderlich
+Date Revised: 4/13/25
+Purpose: model to handle calendar items in database
+*/
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS_Project_Manager.Models 
+{
+    public class MeetingMinutes // creates meeting minutes class that passes in object id, notes, and associated meeting id
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; } // ObjectId identifier in Mongo
+
+        [Required]
+        public required string Notes { get; set; } // Notes for the given meeting
+ 
+        [BsonElement("AssocMeetingId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId AssocMeetingId { get; set; } // Associated meeting ObjectId with notes
+    }
+}
