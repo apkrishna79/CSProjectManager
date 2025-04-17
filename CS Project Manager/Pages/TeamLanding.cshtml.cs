98fb0c2e2c2e0fd53aa7aaed8e08d49cb78514ae
JazzyFierce <102490072+JazzyFierce@users.noreply.github.com>
2025-04-17 10:53:34 -0500

    parse team id

--- CS Project Manager/Pages/TeamLanding.cshtml.cs
+++ CS Project Manager/Pages/TeamLanding.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MongoDB.Bson;
 
 namespace CS_Project_Manager.Pages
 {
     public class TeamLandingModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public ObjectId TeamId { get; set; }
+
+        public void OnGet(string teamId)
         {
+            if (!ObjectId.TryParse(teamId, out ObjectId parsedTeamId))
+            {
+                throw new ArgumentException("Invalid Team ID format");
+            }
+            TeamId = parsedTeamId;
         }
     }
 }
