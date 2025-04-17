5d32b9cc9f477f9c1e0d11c8d3469b42fcc0c01d
JazzyFierce <102490072+JazzyFierce@users.noreply.github.com>
2025-04-17 11:03:10 -0500

    Forbid access to team landing page from non-members

--- CS Project Manager/Pages/TeamLanding.cshtml.cs
+++ CS Project Manager/Pages/TeamLanding.cshtml.cs
@@ -1,21 +1,34 @@
+using CS_Project_Manager.Models;
+using CS_Project_Manager.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MongoDB.Bson;
+using System.Security.Claims;
 
 namespace CS_Project_Manager.Pages
 {
-    public class TeamLandingModel : PageModel
+    public class TeamLandingModel(TeamService teamService, StudentUserService studentUserService) : PageModel
     {
+        private readonly TeamService _teamService = teamService;
+        private readonly StudentUserService _studentUserService = studentUserService;
+
         [BindProperty]
         public ObjectId TeamId { get; set; }
 
-        public void OnGet(string teamId)
+        public async Task OnGetAsync(string teamId)
         {
             if (!ObjectId.TryParse(teamId, out ObjectId parsedTeamId))
             {
                 throw new ArgumentException("Invalid Team ID format");
             }
             TeamId = parsedTeamId;
+
+            string currentEmail = User.FindFirstValue(ClaimTypes.Email);
+            StudentUser currentUser = await _studentUserService.GetUserByEmailAsync(currentEmail);
+            if (!(await _teamService.GetTeamByIdAsync(TeamId)).Members.Contains(currentUser.Id))
+            {
+                throw new UnauthorizedAccessException("You do not have access to this team.");
+            }
         }
     }
 }
